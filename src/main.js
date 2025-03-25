a885c5b51f94205ae3854ffc4aa637f3c3700bae
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-03-25 10:42:55 +0100

    Implement prime numbers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { factorial } from "./maths-algorithms/factorial";
 import { fibonacci } from "./maths-algorithms/fibonacci";
+import { isPrime } from "./maths-algorithms/isPrime";
 
 console.log(fibonacci(2));
 console.log(fibonacci(3));
@@ -9,3 +10,7 @@ console.log(factorial(0));
 console.log(factorial(3));
 console.log(factorial(4));
 console.log(factorial(5));
+console.log(isPrime(1));
+console.log(isPrime(4));
+console.log(isPrime(5));
+console.log(isPrime(7));
