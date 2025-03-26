4cb7f89241c3b013643def7626408348f25ed7f7
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-03-26 12:45:41 +0100

    Implement recursive binary search

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { binarySearch } from "./search-algorithms/binarySearch";
+import { recursiveBinarySearch } from "./search-algorithms/recursiveBinarySearch";
 
-
-console.log(binarySearch([-5, 2, 4, 6, 10], 6));
-console.log(binarySearch([-5, 2, 4, 6, 10], 10));
-console.log(binarySearch([-5, 2, 4, 6, 10], 20));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20));
